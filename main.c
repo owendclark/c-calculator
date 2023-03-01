79aa0fb236785d569c30e739e93ede080b3cdc7f
owendclark3 <owen@perspectiveapplication.com>
2023-03-01 16:17:00 -0500

    Bug fixes
    Switched most data types to floats, fixed operation scanf to include whitespace. Added better error handling.

--- main.c
+++ main.c
@@ -1,40 +1,47 @@
 #include <stdio.h>
 
-int Calculate(int num1, int num2, char operation);
+float Calculate(float num1, float num2, char operation);
 
 int main(void)
 {
-  int userNum1, userNum2, result;
+  float userNum1, userNum2, result;
   char operation;
 
   printf("Enter first number:\n");
-  scanf("%d", &userNum1);
+  scanf("%f", &userNum1);
 
   printf("Enter second number:\n");
-  scanf("%d", &userNum2);
+  scanf("%f", &userNum2);
 
   printf("Enter operation (+, -, *, /):\n");
-  scanf("%c", &operation);
+  scanf(" %c", &operation);
 
   result = Calculate(userNum1, userNum2, operation);
 
-  printf("Result: %d\n", result);
+  if (result == -1)
+  {
+    // Error occured
+    return 1;
+  }
+
+  printf("Result: %.2f\n", result);
 
   return 0;
 }
 
-Calculate(int num1, int num2, char operation)
+float Calculate(float num1, float num2, char operation)
 {
-  int result;
+  float result = 0;
 
   if (operation == '/' && num2 == 0)
   {
     printf("Division by zero is undefined. Please enter a non-zero second number.\n");
-    return result;
+    result = -1;
   }
-
-  switch(operation)
+  else
   {
+    switch (operation)
+    {
     case '+':
       result = num1 + num2;
       break;
@@ -49,7 +56,9 @@ Calculate(int num1, int num2, char operation)
       break;
     default:
       printf("Invalid operation. Please enter a valid operation (+, -, *, /).\n");
+      result = -1;
       break;
+    }
   }
 
   return result;
