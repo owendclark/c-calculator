ba91ab568a1e9a0dd0d93e5515b2aafc59cde3d0
owendclark3 <owen@perspectiveapplication.com>
2023-03-07 12:31:50 -0500

    Bug Fixes, Comments, README
    - Fixed bugs
    - Added input validation
    - NAN for errors
    - Commented code
    - README update

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-A simple calculator that can perform basic arithmetic operations such as addition, subtraction, multiplication, and division. Built in C as a personal project to grow my C experience.
+# Calculator Program
+This is a simple calculator program written in C. The program takes two integer inputs and an arithmetic operator from the user and performs the corresponding arithmetic operation. The available operators are addition (+), subtraction (-), multiplication (*), and division (/).
+
+## Usage Instructions
+1. Clone or download the repository.
+2. Navigate to the project directory.
+3. Compile the program using a C compiler of your choice (e.g., gcc).
+4. Run the compiled program in the terminal or command prompt.
+5. Enter the first integer number and press Enter.
+6. Enter the second integer number and press Enter.
+7. Enter the arithmetic operator (+, -, *, /) and press Enter.
+8. The program will output the result of the arithmetic operation.
+
+**Note:** If you enter an invalid operator or try to divide by zero, the program will display an error message and ask you to enter valid input.
+
+## Example Usage
+```console
+
+$ gcc calculator.c -o calculator
+$ ./calculator
+Enter first number:
+10
+Enter second number:
+5
+Enter operation (+, -, *, /):
+*
+Result: 50
+
+```
--- main.c
+++ main.c
@@ -1,43 +1,63 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <math.h>
+#include <string.h>
 
-float Calculate(float num1, float num2, char operation);
+double Calculate(double num1, double num2, char operation);
 
 int main(void)
 {
-  float userNum1, userNum2, result;
+  double userNum1, userNum2, result;
   char operation;
 
   printf("Enter first number:\n");
-  scanf("%f", &userNum1);
-
-  printf("Enter second number:\n");
-  scanf("%f", &userNum2);
-
-  printf("Enter operation (+, -, *, /):\n");
-  scanf(" %c", &operation);
-
-  result = Calculate(userNum1, userNum2, operation);
-
-  if (result == -__FLT_MAX__)
+  //Validates Input
+  if (scanf("%lf", &userNum1) != 1)
   {
-    // Error occured
-    printf("Error: Result out of range.\n");
+    printf("Error: Invalid input.\n");
     return 1;
   }
 
-  printf("Result: %.2f\n", result);
+  printf("Enter second number:\n");
+  //Validates Input
+  if (scanf("%lf", &userNum2) != 1)
+  {
+    printf("Error: Invalid input.\n");
+    return 1;
+  }
+
+  printf("Enter operation (+, -, *, /):\n");
+  //Validates Input
+  if (scanf(" %c", &operation) != 1 || !strchr("+-*/", operation))
+  {
+    printf("Error: Invalid operation.\n");
+    return 1;
+  }
+
+  result = Calculate(userNum1, userNum2, operation);
+
+  if (isnan(result))
+  {
+    // Error occured
+    printf("Error: Invalid calculation.\n");
+    return 1;
+  }
+
+  printf("Result: %lf\n", result);
 
   return 0;
 }
 
-float Calculate(float num1, float num2, char operation)
+// Runs calculation given the two numbers and the operation, and returns the result or NAN if error
+double Calculate(double num1, double num2, char operation)
 {
-  float result = 0;
+  double result = 0;
 
   if (operation == '/' && num2 == 0)
   {
     printf("Division by zero is undefined. Please enter a non-zero second number.\n");
-    result = -__FLT_MAX__;
+    return NAN;
   }
   else
   {
@@ -54,11 +74,15 @@ float Calculate(float num1, float num2, char operation)
       break;
     case '/':
       result = num1 / num2;
+      // Overflow protection
+      if (!isinf(result) && fabs(result - (double)((long)result)) > 1e-9)
+      {
+        return NAN;
+      }
       break;
     default:
       printf("Invalid operation. Please enter a valid operation (+, -, *, /).\n");
-      result = -__FLT_MAX__;
-      break;
+      return NAN;
     }
   }
 
